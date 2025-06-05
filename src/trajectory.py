38d82e81e4452e61f124e4092302b4e7c8d7b0ee
Ziyad Rahman <zrahman3004@gmail.com>
2025-06-05 09:42:58 -0400

    created trajectory.py

---
+++ src/trajectory.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+def trajectory(arr : np.array) -> np.array:
+    """
+    Dummy function to move onto other parts of the code.
+
+    Args:
+        arr (np.array): Input array of shape (num_frames, height, width).
+
+    Returns:
+        arr (np.array): Array with the trajectory computed.
+    """
+    return arr
