ec48ca3ca5c9b2b863b46f9f510f60ec145955b8
Ziyad Rahman <zrahman3004@gmail.com>
2025-06-05 10:33:06 -0400

    outlined cli

--- src/client.py
+++ src/client.py
@@ -1,1 +1,28 @@
 import click
+from defaults import default_process, default_flow, default_trajectory
+import src.tiffstack as ts
+
+@click.group()
+def cf():
+    """Command line interface for CellFlow."""
+    pass
+
+@cf.group()
+def init():
+    """Initialize CellFlow."""
+    pass
+
+@cf.group()
+def optical_flow():
+    """Optical flow related commands."""
+    pass
+
+@cf.group()
+def trajectory():
+    """Trajectory related commands."""
+    pass
+
+@cf.group()
+def visualize():
+    """Visualization related commands."""
+    pass
