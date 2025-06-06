33df4b3a25012e8bd0a844bdfdd7532e2ed5869e
Ziyad Rahman <zrahman3004@gmail.com>
2025-06-06 13:21:15 -0400

    added video command

--- src/client/video_cmd.py
+++ src/client/video_cmd.py
@@ -1,17 +1,12 @@
 import os
 import click
 from src.memory import main_path, load_metadata, save_video
+from src.client.cli_utils import user_stack_input
 
 @click.command()
 def video():
     """Visualization related commands."""
-    stacks = [entry for entry in os.listdir(main_path) if 
-              os.path.isdir(os.path.join(main_path, entry)) and entry != 'types.json']
-
-    click.echo(f"Please select a stack to visualize from. Please enter only the number.")
-    for i in range(len(stacks)):
-        click.echo(f"{[i]}: {stacks[i]}")
-    stack_index = click.prompt("Enter the number of the stack: ", type=int)
+    stacks, stack_index = user_stack_input()
     stack_path = os.path.join(main_path, stacks[stack_index])
     if 0 <= stack_index < len(stacks):
         ftag = click.prompt(f"You selected: {stacks[stack_index]}. Please type the file tag.")
